504d3ad1a0c8c12a707f0c44174af217df90df34
demog <dg08gg@mail.ru>
2022-08-14 20:54:54 +0300

    Ограничение на карты (криво, но иначе никак)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     suits: ['H', 'D', 'C', 'S'],
     usedRanks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     usedSuits: [0, 0, 0, 0],
+    fullArrs: 0,
+    empty: false
   }
   Card = (props) => {
     let rank = props.rank
@@ -44,8 +46,10 @@ class App extends Component {
         color = 'N'
     }
     const rankClass = (rank) => {
-      if (isNaN(rank)) {
+      if (isNaN(rank) && rank != undefined) {
         return rank.toLowerCase();
+      } else if (rank == undefined) {
+        return 'Закончилось'
       } else {
         return Number(rank)
       }
@@ -58,21 +62,57 @@ class App extends Component {
     )
   }
   randomCards = () => {
-    if (this.state.cards.length !== 0 || this.state.text !== 'Карты закончились') {
-      const cards = this.state.cards;
-      const usedRanks = this.state.usedRanks;
-      const usedSuits = this.state.usedSuits
-      let text = this.state.text;
-      cards.length = 0;
-      text = '';
+    const cards = this.state.cards;
+    const usedRanks = this.state.usedRanks;
+    const usedSuits = this.state.usedSuits;
+    let fullArrs = this.state.fullArrs;
+    let empty = this.state.empty
+    let text = this.state.text;
+    cards.length = 0;
+    text = '';
+    this.setState({
+      text
+    })
+    const suits = this.state.suits;
+    const ranks = this.state.ranks;
+    if (fullArrs === 17 || this.state.suits.length === 0 || this.state.ranks.length === 0) {
+      text = 'Нет карт'
+      empty = true
       this.setState({
         text
       })
-      const suits = this.state.suits;
-      const ranks = this.state.ranks;
-      for (let i = 0; i < 5; i++) {
+    } else {
+      for (let i = 0; i < usedSuits.length + 1; i++) {
+        console.log(1)
         let randSuitNum = Math.floor(Math.random() * suits.length);
         let randRankNum = Math.floor(Math.random() * ranks.length);
+        if (usedRanks[randRankNum] >= 4) {
+          console.log(2)
+          ranks.splice(randRankNum, 1)
+          fullArrs++
+          let oldRandRankNum = randRankNum;
+          if (ranks[oldRandRankNum] === ranks[randRankNum] && this.state.suits.length !== 1 && this.state.ranks.length !== 1) {
+            console.log(3)
+            randRankNum = Math.floor(Math.random() * ranks.length);
+          }
+          this.setState({
+            ranks
+          })
+        }
+        if (usedSuits[randSuitNum] >= 13) {
+          console.log(2)
+          suits.splice(randSuitNum, 1)
+          fullArrs++
+          let oldRandSuitNum = randSuitNum;
+          if (ranks[oldRandSuitNum] === ranks[randSuitNum] && this.state.suits.length !== 1 && this.state.ranks.length !== 1) {
+            console.log(3)
+            randSuitNum = Math.floor(Math.random() * ranks.length);
+          }
+          randSuitNum = Math.floor(Math.random() * suits.length);
+          this.setState({
+            suits
+          })
+        }
         let randRank = ranks[randRankNum];
         let randSuit = suits[randSuitNum];
         cards.push({rank: randRank, suit: randSuit})
@@ -85,143 +125,142 @@ class App extends Component {
           ranks
         })
       }
-      this.setState({
-        cards
-      })
-      this.arm()
     }
+    this.setState({
+      cards,
+      empty,
+      fullArrs
+    })
+    this.arm()
   }
   detecter = (massRank, massSuit) => {
-    if (this.state.cards.length !== 0 || this.state.text !== 'Карты закончились') {
-      let text = this.state.text;
-      const text1 = 'Одна пара';
-      const text2 = 'Две пары';
-      const text3 = 'Тройка';
-      const text4 = 'Покер';
-      const text5 = 'Фулл-хаус';
-      const text6 = 'Флэш';
-      const text7 = 'Стрит';
-      const text8 = 'Стрит-флэш';
-      const text9 = 'Роял-флэш'
-      const rank5A = massRank[0];
-      const rank5B = massRank[3];
-      let rank5Bbull = false;
-      let rank5Abull = false;
-      let namesRank = {};
-      let namesSuit = {};
-      function count(array, choice){
-        if (choice === 1) {
-          array.forEach(item => {
-            namesRank[item] = (namesRank[item] || 0) + 1;
-          });
-        } else if (choice === 2) {
-          array.forEach(item => {
-            namesSuit[item] = (namesRank[item] || 0) + 1;
-          })
-        }
+    let text = this.state.text;
+    const empty = this.state.empty;
+    const text1 = 'Одна пара';
+    const text2 = 'Две пары';
+    const text3 = 'Тройка';
+    const text4 = 'Покер';
+    const text5 = 'Фулл-хаус';
+    const text6 = 'Флэш';
+    const text7 = 'Стрит';
+    const text8 = 'Стрит-флэш';
+    const text9 = 'Роял-флэш'
+    const rank5A = massRank[0];
+    const rank5B = massRank[3];
+    let rank5Bbull = false;
+    let rank5Abull = false;
+    let namesRank = {};
+    let namesSuit = {};
+    function count(array, choice){
+      if (choice === 1) {
+        array.forEach(item => {
+          namesRank[item] = (namesRank[item] || 0) + 1;
+        });
+      } else if (choice === 2) {
+        array.forEach(item => {
+          namesSuit[item] = (namesRank[item] || 0) + 1;
+        })
       }
-      count(massRank, 1)
-      count(massSuit, 2)
-      console.log(namesRank)
-      console.log(namesSuit)
-      let repeatsRank = 0;
-      let repeatsSuit = 0;
-      for (let i in namesRank) {
-        if (namesRank[i] === 2) {
-          repeatsRank++
-        } else if (namesRank[i] === 3) {
-          repeatsRank = 3
-        } else if (namesRank[i] === 4) {
-          repeatsRank = 4
-        }
-      }
-      let suitsLength = 0;
-      for (let i in namesSuit) {
-        console.log(i)
-        suitsLength++
-      }
-      for (let i in namesSuit) {
-        if (namesSuit[i] === 1 && suitsLength === 1) {
-          repeatsSuit++
-        }
-      }
-      if (repeatsRank === 1) {
-        text = text1;
-      } else if (repeatsRank === 2) {
-        text = text2;
-      } else if (repeatsRank === 3) {
-        text = text3;
-      } else if (repeatsRank === 4) {
-        text = text4;
-      } else {
-        text = '';
-      }
-      for (let i = 0; i < 3; i++) {
-        if (massRank[i] === rank5A) {
-          rank5Abull = true;
-        } else {
-          rank5Abull = false;
-          i += 99
-        }
-      }
-      for (let i = 3; i < 5; i++) {
-        if (massRank[i] === rank5B) {
-          rank5Bbull = true;
-        } else {
-          rank5Bbull = false;
-          i += 99
-        }
-      }
-      if (rank5Abull && rank5Bbull) {
-        text = text5;
-      }
-      console.log(repeatsSuit)
-      if (repeatsSuit === 1) {
-        text = text6
-      }
-      let fourNumberArr = false;
-      let fullNumberArr = false;
-      massRank.sort()
-      for (let i = 0; i < massRank.length - 1; i++) {
-        if (!isNaN(massRank[i])) {
-          fourNumberArr = true
-        } else if (isNaN(massRank[i]) || massRank[i] === 10) {
-          fourNumberArr = false;
-          i += 99
-        }
-      }
-      for (let i = 0; i < massRank.length; i++) {
-        if (!isNaN(massRank[i])) {
-          fullNumberArr = true
-        } else if (isNaN[massRank[i]] || massRank[i] === 10) {
-          fullNumberArr = false;
-          i += 99
-        }
-      }
-      console.log(massRank[0] + 1 === massRank[1])
-      if (fourNumberArr && massRank[0] + 1 === massRank[1] && massRank[1] + 1 === massRank[2] && massRank[2] + 1 === massRank[3] && massRank[4] === 'J') {
-        text = text7
-      }
-      let sampleSuit = massSuit[0];
-      let oneSuitArr = false;
-      for (let i = 0; i < massSuit.length; i++) {
-        if (massSuit[i] === sampleSuit) {
-          oneSuitArr = true
-        } else if (massSuit[i] !== sampleSuit) {
-          oneSuitArr = false;
-          i += 99
-        }
-      }
-      if (oneSuitArr && fullNumberArr && massRank[0] + 1 === massRank[1] && massRank[1] + 1 === massRank[2] && massRank[2] + 1 === massRank[3] && massRank[3] + 1 === massRank[4]) {
-        text = text8
-      }
-      if (oneSuitArr && massRank[0] === 10 && massRank[1] === 'A' && massRank[2] === 'J' && massRank[3] === 'K' && massRank[4] === 'Q') {
-        text = text9
-      }
-      this.setState({
-        text
-      })
     }
+    count(massRank, 1)
+    count(massSuit, 2)
+    let repeatsRank = 0;
+    let repeatsSuit = 0;
+    for (let i in namesRank) {
+      if (namesRank[i] === 2) {
+        repeatsRank++
+      } else if (namesRank[i] === 3) {
+        repeatsRank = 3
+      } else if (namesRank[i] === 4) {
+        repeatsRank = 4
+      }
+    }
+    let suitsLength = 0;
+    for (let i in namesSuit) {
+      suitsLength++
+    }
+    for (let i in namesSuit) {
+      if (namesSuit[i] === 1 && suitsLength === 1) {
+        repeatsSuit++
+      }
+    }
+    if (repeatsRank === 1) {
+      text = text1;
+    } else if (repeatsRank === 2) {
+      text = text2;
+    } else if (repeatsRank === 3) {
+      text = text3;
+    } else if (repeatsRank === 4) {
+      text = text4;
+    } else {
+      text = '';
+    }
+    for (let i = 0; i < 3; i++) {
+      if (massRank[i] === rank5A) {
+        rank5Abull = true;
+      } else {
+        rank5Abull = false;
+        i += 99
+      }
+    }
+    for (let i = 3; i < 5; i++) {
+      if (massRank[i] === rank5B) {
+        rank5Bbull = true;
+      } else {
+        rank5Bbull = false;
+        i += 99
+      }
+    }
+    if (rank5Abull && rank5Bbull) {
+      text = text5;
+    }
+    if (repeatsSuit === 1) {
+      text = text6
+    }
+    let fourNumberArr = false;
+    let fullNumberArr = false;
+    massRank.sort()
+    for (let i = 0; i < massRank.length - 1; i++) {
+      if (!isNaN(massRank[i])) {
+        fourNumberArr = true
+      } else if (isNaN(massRank[i]) || massRank[i] === 10) {
+        fourNumberArr = false;
+        i += 99
+      }
+    }
+    for (let i = 0; i < massRank.length; i++) {
+      if (!isNaN(massRank[i])) {
+        fullNumberArr = true
+      } else if (isNaN[massRank[i]] || massRank[i] === 10) {
+        fullNumberArr = false;
+        i += 99
+      }
+    }
+    if (fourNumberArr && massRank[0] + 1 === massRank[1] && massRank[1] + 1 === massRank[2] && massRank[2] + 1 === massRank[3] && massRank[4] === 'J') {
+      text = text7
+    }
+    let sampleSuit = massSuit[0];
+    let oneSuitArr = false;
+    for (let i = 0; i < massSuit.length; i++) {
+      if (massSuit[i] === sampleSuit) {
+        oneSuitArr = true
+      } else if (massSuit[i] !== sampleSuit) {
+        oneSuitArr = false;
+        i += 99
+      }
+    }
+    if (oneSuitArr && fullNumberArr && massRank[0] + 1 === massRank[1] && massRank[1] + 1 === massRank[2] && massRank[2] + 1 === massRank[3] && massRank[3] + 1 === massRank[4]) {
+      text = text8
+    }
+    if (oneSuitArr && massRank[0] === 10 && massRank[1] === 'A' && massRank[2] === 'J' && massRank[3] === 'K' && massRank[4] === 'Q') {
+      text = text9
+    }
+    if (empty || this.state.ranks.length === 0 || this.state.suits.length === 0) {
+      text = 'Карты закончились'
+    }
+    this.setState({
+      text
+    })
   }
   arm = () => {
     if (!this.state.empty) {
