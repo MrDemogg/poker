7a72ff6831d62998cbc5dcb215df875758b83aa2
demog <dg08gg@mail.ru>
2022-08-14 15:53:46 +0300

    Распознание флеш руки, удаление ограниченных карт

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends Component {
   state = {
     cards: [],
     text: '',
+    ranks: ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2],
+    suits: ['H', 'D', 'C', 'S'],
     usedRanks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    empty: false
+    usedSuits: [0, 0, 0, 0],
   }
   Card = (props) => {
     let rank = props.rank
@@ -56,51 +58,32 @@ class App extends Component {
     )
   }
   randomCards = () => {
-    if (!this.state.empty || this.state.text !== 'Нету') {
+    if (this.state.cards.length !== 0 || this.state.text !== 'Карты закончились') {
       const cards = this.state.cards;
       const usedRanks = this.state.usedRanks;
+      const usedSuits = this.state.usedSuits
       let text = this.state.text;
       cards.length = 0;
       text = '';
       this.setState({
         text
       })
-      const suits = ['H', 'D', 'C', 'S'];
-      const ranks = ['A', 'K', 'Q', 'J', 10, 9, 8, 7, 6, 5, 4, 3, 2]
-      let empty = this.state.empty;
-      let research = true
+      const suits = this.state.suits;
+      const ranks = this.state.ranks;
       for (let i = 0; i < 5; i++) {
         let randSuitNum = Math.floor(Math.random() * suits.length);
         let randRankNum = Math.floor(Math.random() * ranks.length);
-        console.log(randRankNum)
-        while (usedRanks[randRankNum] == 4 && research) {
-          if (ranks.length == 0) {
-            ranks.push('нету')
-            randRankNum = 0;
-            text = 'Карты закончились'; 
-            empty = true;
-            research = false
-            this.setState({
-              text,
-              empty
-            })
-          } else {
-            ranks.splice(randRankNum, 1)
-            randRankNum = Math.floor(Math.random() * ranks.length);
-            console.log(randRankNum)
-            console.log(ranks)
-          }
-          this.setState({
-            usedRanks
-          })
-        }
         let randRank = ranks[randRankNum];
-        if (!research) {
-          break;
-        }
         let randSuit = suits[randSuitNum];
         cards.push({rank: randRank, suit: randSuit})
         usedRanks[randRankNum]++
+        usedSuits[randSuitNum]++
+        this.setState({
+          usedRanks,
+          usedSuits,
+          suits,
+          ranks
+        })
       }
       this.setState({
         cards
@@ -108,86 +91,106 @@ class App extends Component {
       this.arm()
     }
   }
-  detecter = (massRank) => {
-    console.log('one')
-    let text = this.state.text;
-    const empty = this.state.empty;
-    const text1 = 'Одна пара';
-    const text2 = 'Две пары';
-    const text3 = 'Тройка';
-    const text4 = 'Покер';
-    const text5 = 'Фулл-хаус';
-    const rank5A = massRank[0];
-    const rank5B = massRank[3];
-    let rank5Bbull = false;
-    let rank5Abull = false;
-    let names = {};
-    function count(array){
-      array.forEach(item => {
-        names[item] = (names[item] || 0) + 1;
-      });
-    }
-    count(massRank)
-    console.log(names)
-    let duo = 0;
-    for (let i in names) {
-      console.log(names[i])
-      if (names[i] == 2) {
-        duo++
-      } else if (names[i] == 3) {
-        duo = 3
-      } else if (names[i] == 4) {
-        duo = 4
+  detecter = (massRank, massSuit) => {
+    if (this.state.cards.length !== 0 || this.state.text !== 'Карты закончились') {
+      let text = this.state.text;
+      const text1 = 'Одна пара';
+      const text2 = 'Две пары';
+      const text3 = 'Тройка';
+      const text4 = 'Покер';
+      const text5 = 'Фулл-хаус';
+      const text6 = 'Флэш';
+      const rank5A = massRank[0];
+      const rank5B = massRank[3];
+      let rank5Bbull = false;
+      let rank5Abull = false;
+      let namesRank = {};
+      let namesSuit = {};
+      function count(array, choice){
+        if (choice === 1) {
+          array.forEach(item => {
+            namesRank[item] = (namesRank[item] || 0) + 1;
+          });
+        } else if (choice === 2) {
+          array.forEach(item => {
+            namesSuit[item] = (namesRank[item] || 0) + 1;
+          })
+        }
       }
-    }
-    if (duo == 1) {
-      text = text1;
-    } else if (duo == 2) {
-      text = text2;
-    } else if (duo == 3) {
-      text = text3;
-    } else if (duo == 4) {
-      text = text4;
-    } else {
-      text = '';
-    }
-    for (let i = 0; i < 3; i++) {
-      if (massRank[i] == rank5A) {
-        rank5Abull = true;
+      count(massRank, 1)
+      count(massSuit, 2)
+      console.log(namesRank)
+      console.log(namesSuit)
+      let repeatsRank = 0;
+      let repeatsSuit = 0;
+      for (let i in namesRank) {
+        if (namesRank[i] === 2) {
+          repeatsRank++
+        } else if (namesRank[i] === 3) {
+          repeatsRank = 3
+        } else if (namesRank[i] === 4) {
+          repeatsRank = 4
+        }
+      }
+      let suitsLength = 0;
+      for (let i in namesSuit) {
+        suitsLength++
+      }
+      for (let i in namesSuit) {
+        if (namesSuit[i] === 1 && suitsLength === 1) {
+          repeatsSuit++
+        }
+      }
+      if (repeatsRank === 1) {
+        text = text1;
+      } else if (repeatsRank === 2) {
+        text = text2;
+      } else if (repeatsRank === 3) {
+        text = text3;
+      } else if (repeatsRank === 4) {
+        text = text4;
       } else {
-        rank5Abull = false;
-        i += 99
+        text = '';
       }
-    }
-    for (let i = 3; i < 5; i++) {
-      if (massRank[i] == rank5B) {
-        rank5Bbull = true;
-      } else {
-        rank5Bbull = false;
-        i += 99
+      for (let i = 0; i < 3; i++) {
+        if (massRank[i] === rank5A) {
+          rank5Abull = true;
+        } else {
+          rank5Abull = false;
+          i += 99
+        }
       }
+      for (let i = 3; i < 5; i++) {
+        if (massRank[i] === rank5B) {
+          rank5Bbull = true;
+        } else {
+          rank5Bbull = false;
+          i += 99
+        }
+      }
+      if (rank5Abull && rank5Bbull) {
+        text = text5;
+      }
+      console.log(repeatsSuit)
+      if (repeatsSuit === 1) {
+        text = text6
+      }
+      this.setState({
+        text
+      })
     }
-    if (rank5Abull && rank5Bbull) {
-      text = text5;
-    }
-    if (empty) {
-      text = 'Нету'
-    }
-    this.setState({
-      text
-    })
-    
   }
   arm = () => {
-    console.log('arm')
-    const cards = this.state.cards
-    const suits = [];
-    const ranks = [];
-    for (let i = 0; i < cards.length; i++) {
-      suits.push(cards[i].suit)
-      ranks.push(cards[i].rank)
+    if (!this.state.empty) {
+      const cards = this.state.cards
+      const suits = [];
+      const ranks = [];
+      for (let i = 0; i < cards.length; i++) {
+        suits.push(cards[i].suit)
+        ranks.push(cards[i].rank)
+      }
+      this.detecter(ranks, suits)
     }
-    this.detecter(ranks)
   }
   render() {
     return (
